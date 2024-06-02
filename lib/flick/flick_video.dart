793a78e6af761d1a61e55bcd4b6643507e5eb93c
ZRShamim <zrshamim8822@gmail.com>
2024-06-02 11:05:03 +0600

    flutter downloader for background download

--- lib/flick/flick_video.dart
+++ lib/flick/flick_video.dart
@@ -1,12 +1,13 @@
 import 'dart:developer';
-import 'dart:io';
-
+import 'dart:isolate';
+import 'dart:ui';
 import 'package:flick_video_player/flick_video_player.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_downloader/flutter_downloader.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:video_player/video_player.dart';
-import 'package:videoplayer/download_file/download_file.dart';
+import 'package:path_provider/path_provider.dart';
 
 class FlickVideo extends StatefulWidget {
   const FlickVideo({super.key});
@@ -18,14 +19,32 @@ class FlickVideo extends StatefulWidget {
 class _FlickVideoState extends State<FlickVideo> {
   late FlickManager flickManager;
   late VideoPlayerController videoPlayerController;
-  String video_url =
+  String videoUrl =
       'https://flutter.github.io/assets-for-api-docs/assets/videos/bee.mp4';
-  //String video_url = "https://www.googleapis.com/drive/v3/files/12QWT-RUDW-m-hYb0wuzdXOtqBmssTqzE?alt=media&key=AIzaSyB_cNFR2Km1RdWkwkeyNo4HgINPguS0_LI",
+  //String videoUrl = "https://www.googleapis.com/drive/v3/files/12QWT-RUDW-m-hYb0wuzdXOtqBmssTqzE?alt=media&key=AIzaSyB_cNFR2Km1RdWkwkeyNo4HgINPguS0_LI",
+
+  String? taskId;
+  final ReceivePort _port = ReceivePort();
 
   double? aspectRatio;
   @override
   void initState() {
     videoController();
+    IsolateNameServer.registerPortWithName(
+        _port.sendPort, 'downloader_send_port');
+    _port.listen((dynamic data) {
+      String id = data[0];
+      DownloadTaskStatus status = data[1];
+      int progress = data[2];
+
+      if (status == DownloadTaskStatus.complete) {
+        log("Download Complete");
+      }
+
+      setState(() {});
+    });
+
+    FlutterDownloader.registerCallback(downloadCallback);
     super.initState();
   }
 
@@ -35,7 +54,7 @@ class _FlickVideoState extends State<FlickVideo> {
 
     videoPlayerController = VideoPlayerController.networkUrl(
       Uri.parse(
-        video_url,
+        videoUrl,
       ),
     )..addListener(() {
         log(videoPlayerController.value.duration.toString());
@@ -57,9 +76,17 @@ class _FlickVideoState extends State<FlickVideo> {
     );
   }
 
+  @pragma('vm:entry-point')
+  static void downloadCallback(String id, int status, int progress) {
+    final SendPort? send =
+        IsolateNameServer.lookupPortByName('downloader_send_port');
+    send!.send([id, status, progress]);
+  }
+
   @override
   void dispose() {
     flickManager.dispose();
+    IsolateNameServer.removePortNameMapping('downloader_send_port');
     super.dispose();
   }
 
@@ -81,8 +108,18 @@ class _FlickVideoState extends State<FlickVideo> {
                 PermissionStatus status = await Permission.storage.request();
                 if (status.isGranted) {
                   log('Downloading');
-                  Downloader.downloadFile(
-                    url: video_url,
+                  // Downloader.downloadFile(
+                  //   url: videoUrl,
+                  // );
+                  final baseStorage = await getExternalStorageDirectory();
+                  taskId = await FlutterDownloader.enqueue(
+                    url: videoUrl,
+                    headers: {}, // optional: header send with url (auth token etc)
+                    savedDir: baseStorage!.path,
+                    showNotification:
+                        true, // show download progress in status bar (for Android)
+                    openFileFromNotification:
+                        true, // click on notification to open downloaded file (for Android)
                   );
                 } else {
                   log("Denied");
--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,10 @@ import "package:flutter/material.dart";
 import 'package:flutter/services.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:videoplayer/flick/flick_video.dart';
+import 'package:flutter_downloader/flutter_downloader.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await GetStorage.init();
   SystemChrome.setSystemUIOverlayStyle(
     const SystemUiOverlayStyle(
@@ -11,6 +13,12 @@ void main() async {
       statusBarColor: Colors.black54, // status bar color
     ),
   );
+  await FlutterDownloader.initialize(
+      debug:
+          true, // optional: set to false to disable printing logs to console (default: true)
+      ignoreSsl:
+          true // option: set to false to disable working with http links (default: false)
+      );
   runApp(const MyApp());
 }
 
